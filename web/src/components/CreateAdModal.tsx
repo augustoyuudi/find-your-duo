a1c37eeceb7ab7093ad5099e975530348a6d9d4e
augustoyuudi <augustoyuudi@gmail.com>
2022-09-17 20:24:30 -0300

    feat: submit create ad form

--- web/src/components/CreateAdModal.tsx
+++ web/src/components/CreateAdModal.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import * as Dialog from '@radix-ui/react-dialog'
 import * as Checkbox from '@radix-ui/react-checkbox'
 import * as Select from '@radix-ui/react-select'
@@ -15,17 +15,89 @@ interface Game {
 }
 
 export function CreateAdModal({ games }: Props) {
+  const weekDays = [
+    {
+      id: 'domingo',
+      label: 'D'
+    },
+    {
+      id: 'segunda',
+      label: 'S'
+    },
+    {
+      id: 'terca',
+      label: 'T'
+    },
+    {
+      id: 'quarta',
+      label: 'Q'
+    },
+    {
+      id: 'quinta',
+      label: 'Q'
+    },
+    {
+      id: 'sexta',
+      label: 'S'
+    },
+    {
+      id: 'sabado',
+      label: 'S'
+    },
+  ]
+
+  const [selectedDays, setSelectedDays] = useState(new Set())
+  const [isVoiceChatSelected, setIsVoiceChatSelected] = useState(false)
+
+  function updateSelectedDays(value: number): void {
+    selectedDays.add(value)
+    setSelectedDays(selectedDays)
+  }
+
+  function toggleVoiceChat() {
+    setIsVoiceChatSelected(!isVoiceChatSelected)
+  }
+
+  async function onSubmit(event: FormEvent) {
+    event.preventDefault()
+
+    const formData = new FormData(event.target as HTMLFormElement)
+    const data = Object.fromEntries(formData)
+
+    // TODO: validate data before form submission
+
+    try {
+      await fetch(`http://localhost:3333/games/${data.gameId}/ads`,{
+        method: 'POST',
+        body: JSON.stringify({
+          ...data,
+          yearsPlaying: Number(data.yearsPlaying),
+          weekDays: Array.from(selectedDays),
+          useVoiceChannel: isVoiceChatSelected
+        }),
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+
+      alert('Formulário enviado com sucesso!')
+    } catch(error) {
+      console.error(error);
+    }
+  }
+
   return (
     <Dialog.Portal>
       <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
 
-      <Dialog.Content className='fixed bg-[#2A2634] px-10 py-8 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[30rem] shadow-lg shadow-black/25'>
+      <Dialog.Content className='fixed bg-[#2A2634] px-10 py-8 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[32rem] shadow-lg shadow-black/25'>
         <Dialog.Title className='text-3xl font-black'>Publique um anúncio</Dialog.Title>
 
-        <form className='mt-8 flex flex-col gap-4'>
+        <form className='mt-8 flex flex-col gap-4' onSubmit={onSubmit}>
           <fieldset className='flex flex-col gap-2'>
             <label htmlFor="game">Qual o game?</label>
-            <Select.Root>
+            <Select.Root name='gameId'>
               <Select.Trigger
                 className={`
                   flex
@@ -61,17 +133,17 @@ export function CreateAdModal({ games }: Props) {
 
           <fieldset className='flex flex-col gap-2'>
             <label htmlFor="name">Seu nome (ou nickname)</label>
-            <Input id='name' placeholder='Como te chamam dentro do game?' />
+            <Input name='name' id='name' placeholder='Como te chamam dentro do game?' />
           </fieldset>
 
           <div className='grid grid-cols-2 gap-6'>
             <fieldset className='flex flex-col gap-2'>
               <label htmlFor="yearsPlaying">Joga há quantos anos?</label>
-              <Input id='yearsPlaying' type="number" placeholder='Tudo bem ser ZERO' />
+              <Input name='yearsPlaying' id='yearsPlaying' type="number" placeholder='Tudo bem ser ZERO' />
             </fieldset>
             <fieldset className='flex flex-col gap-2'>
               <label htmlFor="discord">Qual seu discord?</label>
-              <Input id='discord' type="text" placeholder='Usuário #0000' />
+              <Input name='discord' id='discord' type="text" placeholder='Usuário #0000' />
             </fieldset>
           </div>
 
@@ -79,47 +151,35 @@ export function CreateAdModal({ games }: Props) {
             <fieldset className='flex flex-col gap-2'>
               <label htmlFor="weekDays">Quando costuma jogar?</label>
               <ol className='grid grid-cols-4 gap-2'>
-                <li>
-                  <input id='domingo' type="checkbox" className='hidden peer' />
-                  <label htmlFor="domingo" className='flex items-center justify-center w-8 h-8 rounded bg-zinc-900 peer-checked:bg-violet-500 peer-checked:text-white text-zinc-500'>D</label>
-                </li>
-                <li>
-                  <input id='segunda' type="checkbox" className='hidden peer' />
-                  <label htmlFor="segunda" className='flex items-center justify-center w-8 h-8 rounded bg-zinc-900 peer-checked:bg-violet-500 peer-checked:text-white text-zinc-500'>S</label>
-                </li>
-                <li>
-                  <input id='terca' type="checkbox" className='hidden peer' />
-                  <label htmlFor="terca" className='flex items-center justify-center w-8 h-8 rounded bg-zinc-900 peer-checked:bg-violet-500 peer-checked:text-white text-zinc-500'>T</label>
-                </li>
-                <li>
-                  <input id='quarta' type="checkbox" className='hidden peer' />
-                  <label htmlFor="quarta" className='flex items-center justify-center w-8 h-8 rounded bg-zinc-900 peer-checked:bg-violet-500 peer-checked:text-white text-zinc-500'>Q</label>
-                </li>
-                <li>
-                  <input id='quinta' type="checkbox" className='hidden peer' />
-                  <label htmlFor="quinta" className='flex items-center justify-center w-8 h-8 rounded bg-zinc-900 peer-checked:bg-violet-500 peer-checked:text-white text-zinc-500'>Q</label>
-                </li>
-                <li>
-                  <input id='sexta' type="checkbox" className='hidden peer' />
-                  <label htmlFor="sexta" className='flex items-center justify-center w-8 h-8 rounded bg-zinc-900 peer-checked:bg-violet-500 peer-checked:text-white text-zinc-500'>S</label>
-                </li>
-                <li>
-                  <input id='sabado' type="checkbox" className='hidden peer' />
-                  <label htmlFor="sabado" className='flex items-center justify-center w-8 h-8 rounded bg-zinc-900 peer-checked:bg-violet-500 peer-checked:text-white text-zinc-500'>S</label>
-                </li>
+                {weekDays.map((day, index) => (
+                  <li key={day.id}>
+                    <input
+                      id={day.id}
+                      type="checkbox"
+                      className='hidden peer'
+                      onChange={() => updateSelectedDays(index)}
+                    />
+                    <label htmlFor={day.id} className='flex items-center justify-center w-8 h-8 rounded bg-zinc-900 peer-checked:bg-violet-500 peer-checked:text-white text-zinc-500'>{day.label}</label>
+                  </li>
+                ))}
               </ol>
             </fieldset>
             <fieldset className='flex flex-col gap-2 flex-1'>
               <label htmlFor="startHour">Qual horário do dia?</label>
               <div className='grid grid-cols-2 gap-2'>
-                <Input id='startHour' type="time" placeholder='De' />
-                <Input id='endHour' type="time" placeholder='Até' />
+                <Input name='startHour' id='startHour' type="time" placeholder='De' />
+                <Input name='endHour' id='endHour' type="time" placeholder='Até' />
               </div>
             </fieldset>
           </div>
 
           <fieldset className='mt-2 flex items-center gap-2 text-sm' >
-            <Checkbox.Root id='chat' className='w-6 h-6 p-1 rounded bg-zinc-900'>
+            <Checkbox.Root
+              id='chat'
+              className='w-6 h-6 p-1 rounded bg-zinc-900'
+              checked={isVoiceChatSelected}
+              onCheckedChange={toggleVoiceChat}
+            >
               <Checkbox.Indicator>
                 <Check className="w-4 h-4 text-emerald-400" />
               </Checkbox.Indicator>
